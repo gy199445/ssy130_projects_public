cf3ff64a76871d6c45f2cd1b604b0a52165aad6b
nisimon <nisimon@student.chalmers.se>
2016-11-09 16:36:48 +0100

    Added basic functionality for decoding message

--- project1A.m
+++ project1A.m
@@ -1,17 +1,39 @@
 %% Define constants and channels
 h_1 = 0.8.^(0:59);
 h_2 = [0.5 zeros(1,7) 0.5];
-QPSK_constellation = [1+1j -1+1j 1-1j -1-1j]/(2^0.5);
-N_bits = 256;
-N_cp = 8;
-h_test = [0.8 0 0.15 0.05];
+
+N = 128;
+N_bits = 2*N;
+N_cp = 100;
+
+% choice of channel
+h = h_1;
 %% Generate symbols
-b = randi(2,1,N_bits)-1;
+b = 2*randi(2,1,N_bits)-3;
 %% Generate OFDM symbol
-z = transmitter(b,N_cp);
+z = transmitter(b,N,N_cp);
 %% Transmit OFDM symbol
-y = conv(h_test,z);
-y = awgn(conv(z,h_test),20,'measured');
+% Send through channel
+y = conv(h,z);
+% Add noise
+y = awgn(y,20,'measured');
 %% OFDM decoding
+% discard cyclic prefix
+y = y(N_cp+1:end);
+
+% transform back
+r = fft(y(1:N));
+
+% compute channel gain
+H = fft(h, N);
+
+% multiply received message with channel gain
+s = conj(H).*r(1:N);
+
 %estimate channel
-%decode ofdm symbol
+
+
+
+%decode symbols
+b2 = [sign(real(s)); sign(imag(s))];
+b2 = b2(:)';
