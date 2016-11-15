cbc3ff0c823730f5ac8c62ce246081844235c0c5
Martin Yang <daoyuan@student.chalmers.se>
2016-11-15 18:11:36 +0100

    add delay; add iteration for simulation; change SNR to sigma

--- channel.m
+++ channel.m
@@ -1,8 +1,15 @@
-function [ y ] = channel( z,h,SNR )
+function [ y ] = channel( z,h,sigma,delay )
 %CHANNEL Summary of this function goes here
 %   Detailed explanation goes here
 %   An multipath AWGN channel
 y = conv(z,h);
-awgn(y,SNR,'measured');
+y_len = length(y);
+w = 1/sqrt(2)*sigma*(randn(1,y_len) + 1i*randn(1,y_len));%noise
+y = y+w;
+if delay>0
+    y = y(delay+1:end);
+else
+    y = [zeros(1,delay) y];
+end
 end
 
--- project1A.m
+++ project1A.m
@@ -3,48 +3,54 @@ h_1 = 0.8.^(0:59);
 h_2 = [0.5 zeros(1,7) 0.5];
 N = 128;
 N_bits = 2*N;
-N_cp = 16;
-SNR = 10;
+N_cp = 64;
+sigma = 0;%noise level
 % choice of channel
-h = h_2;
+h = h_1;
 %PN code as pilot:
 L_PN = 256;
 PN_init_cond = [0 1 0 1 0 0 0 1];
 PN_Polynomial = [8 2 0];
 pn = pn_gen(L_PN,PN_init_cond,PN_Polynomial);
-%% Generate symbols
-bits = randsrc(1,N_bits,[0 1]);
-%the previous function is actually generating -1 and 1, they are not bits.
-symbol = bits2sym(bits);
 % Generate symbols for PN sequence
 pn_symbol = bits2sym(pn);
-%% Generate OFDM symbol
-%make the process of generating OFDM symbol clear 
-z = OFDM_gen(symbol,N,N_cp);
-pn_ofdm = OFDM_gen(pn_symbol,N,N_cp);
-pn_z = [pn_ofdm z]; %pack the training symbol to message
-%% Transmit OFDM symbol
-y = channel(z,h,20);%known channel
-pn_y = channel(pn_z,h,SNR);%unknown channel
-%% OFDM decoding (channel known)
-% discard cyclic prefix
-y = y(N_cp+1:end);
-
-% transform back
-r = fft(y,N);
-
-% compute channel gain
-H = fft(h, N);
-
-% multiply received message with channel gain
-s = conj(H).*r;
-
-%decode symbols
-bits_ = sym2bits(s);
-diff_bits = sum(abs(bits - bits_));%difference in bits
-disp(diff_bits)
-%% OFDM decoding (channel unknown)
-[H_, symbol_] = OFDM_equalization(pn_y,pn_symbol,N,N_cp);
-bits_ch_unknown = sym2bits(symbol_);
-diff_bits_ch_unknown = sum(abs(bits - bits_ch_unknown));%difference in bits
-disp(diff_bits_ch_unknown)
+delay = 0;
+%number of iterations
+N_it = 2;
+%bit error counters for each iterations
+bits_err_known_ch = zeros(1,N_it);
+bits_err_unknow_ch = zeros(1,N_it);
+for i=1:N_it
+    %% Generate symbols
+    bits = randsrc(1,N_bits,[0 1]);
+    %the previous function is actually generating -1 and 1, they are not bits.
+    symbol = bits2sym(bits);
+    %% Generate OFDM symbol
+    %make the process of generating OFDM symbol clear
+    z = OFDM_gen(symbol,N,N_cp);
+    pn_ofdm = OFDM_gen(pn_symbol,N,N_cp);
+    pn_z = [pn_ofdm z]; %pack the training symbol to message
+    %% Transmit OFDM symbol
+    y = channel(z,h,sigma,delay);%known channel
+    pn_y = channel(pn_z,h,sigma,delay);%unknown channel
+    %% OFDM decoding (channel known)
+    % discard cyclic prefix
+    y = y(N_cp+1:end);
+    
+    % transform back
+    r = fft(y,N);
+    
+    % compute channel gain
+    H = fft(h, N);
+    
+    % multiply received message with channel gain
+    s = conj(H).*r;
+    
+    %decode symbols
+    bits_ = sym2bits(s);
+    bits_err_known_ch(i) = sum(abs(bits - bits_));%difference in bits
+    %% OFDM decoding (channel unknown)
+    [~, symbol_] = OFDM_equalization(pn_y,pn_symbol,N,N_cp);
+    bits_ch_unknown = sym2bits(symbol_);
+    bits_err_unknow_ch(i) = sum(abs(bits - bits_ch_unknown));%difference in bits
+end
