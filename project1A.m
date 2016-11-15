1202fbf52c0887e8ddfd88fd6968c58584f042d4
Simon Nilsson <nisimon@student.chalmers.se>
2016-11-15 16:37:21 +0100

    Added functionality for channel estimation

--- project1A.m
+++ project1A.m
@@ -8,31 +8,44 @@ N_cp = 100;
 
 % choice of channel
 h = h_1;
+
 %% Generate symbols
 b = 2*randi(2,1,N_bits)-3;
-%% Generate OFDM symbol
+
+%% Generate training sequence
+b_train = 2*randi(2,1,N_bits)-3;
+s_train = bits2sym(b_train);
+
+%% Generate OFDM symbols
 z = transmitter(b,N,N_cp);
+z_train = transmitter(b_train,N,N_cp);
+
 %% Transmit OFDM symbol
 % Send through channel
 y = conv(h,z);
+y_train = conv(h,z_train);
+
 % Add noise
 y = awgn(y,20,'measured');
+y_train = awgn(y_train, 20, 'measured');
+
 %% OFDM decoding
 % discard cyclic prefix
 y = y(N_cp+1:end);
+y_train = y_train(N_cp+1:end);
 
 % transform back
 r = fft(y,N);
+r_train = fft(y_train,N);
+
+%estimate channel
+H = r_train./s_train;
 
 % compute channel gain
-H = fft(h, N);
+%H = fft(h, N);
 
 % multiply received message with channel gain
 s = conj(H).*r;
-
-%estimate channel
-
-
 
 %decode symbols
 b2 = [sign(real(s)); sign(imag(s))];
