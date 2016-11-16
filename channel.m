92c843ca98d642c9d53d5278c81b316ea0a329be
nisimon <nisimon@student.chalmers.se>
2016-11-16 12:08:56 +0100

    Add delay by function circshift

--- channel.m
+++ channel.m
@@ -6,9 +6,7 @@ y = conv(z,h);
 y_len = length(y);
 w = 1/sqrt(2)*sigma*(randn(1,y_len) + 1i*randn(1,y_len));%noise
 y = y+w;
-if delay>=0
-    y = [zeros(1,delay) y];
-else
-    y = y(abs(delay)+1:end);
-end
+
+% add delay to channel, simulating synchronization delay
+y = circshift(y,[0, -delay]);
 end
