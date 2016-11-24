196f27970aace86f08b3a24b95e0c50c141f7e6c
nisimon <nisimon@student.chalmers.se>
2016-11-24 22:11:18 +0100

    Updated equalization to use row vectors

--- OFDM_equalization.m
+++ OFDM_equalization.m
@@ -14,7 +14,7 @@ y_train = y(N_cp+1:N_cp+N);
 %Data symbol  
 y_data = y((N_cp+N)+N_cp+1:(N_cp+N)+N_cp+N);
 %estimate channel
-H_hat = fft(y_train,N)./(pn_symbol.');% avoid dividing zero
+H_hat = fft(y_train,N)./(pn_symbol);
 %equalization (zero forcing)
 symbol = conj(H_hat).*fft(y_data);
 end
