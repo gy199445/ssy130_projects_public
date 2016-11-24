682c348e9ec3b6a513836ecdc6e9cd4aa29aa1b2
nisimon <nisimon@student.chalmers.se>
2016-11-24 22:11:33 +0100

    Updated equalization ensure negative delay

--- OFDM_equalization.m
+++ OFDM_equalization.m
@@ -9,10 +9,13 @@ function [ H_hat, symbol ] = OFDM_equalization( y,pn_symbol,N,N_cp )
 %   N_cp: length of cp
 %   H: estimated channel
 %   symbol: equalized data symbol
+
+% Make sure delay is negative
+N_delay = floor(N_cp*0.8);
 %Training symbol
-y_train = y(N_cp+1:N_cp+N);
+y_train = y(N_delay+1:N_delay+N);
 %Data symbol  
-y_data = y((N_cp+N)+N_cp+1:(N_cp+N)+N_cp+N);
+y_data = y((N_delay+N)+N_cp+1:(N_delay+N)+N_cp+N);
 %estimate channel
 H_hat = fft(y_train,N)./(pn_symbol);
 %equalization (zero forcing)
